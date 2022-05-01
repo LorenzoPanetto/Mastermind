d5bb267a984b5cd410acbbe02835f2d54fab3cd5
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-05-01 20:31:06 +0200

    Update mastermind.cpp

--- mastermind.cpp
+++ mastermind.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <conio.h>
+#include <locale>
 using namespace std;
 // g++ mastermind.cpp -o mastermind.exe
 
@@ -18,9 +19,10 @@ void SetColor(short Color) {
 }
 
 
-struct Variabile{
-	int numeri;
-	int colori;
+struct utente{
+	string nome;
+    int anni;
+    int mod;
 };
 
 
@@ -34,7 +36,6 @@ bool duplicates(int sec[], int dim) {
     }
     return false;
 }
-
 
 
 //creazione codice segreto PC @@bulfoni
@@ -60,7 +61,9 @@ void secretCodeColor(int sec[], int dim) {
     }
     while (duplicates(sec, dim)==true) {
         for (int i=0; i<dim; i++) {
-			sec[i]=rand()%8;      	
+			sec[i]=rand()%8;
+			while (sec[i]==0)
+				sec[i]=rand()%8;     	
         }
     }
     
@@ -141,7 +144,6 @@ bool check (int code[], int sec[], int dim) {
 }
 
 
-
 //indica quanti numeri giusti in posizione sbagliata ci sono nel codice dell'utente rispetto al pc
 int numGiusti (int code[], int sec[], int dim) {
 	int num=0; //numeri giusti in posizione sbagliata
@@ -168,8 +170,6 @@ int posGiusti (int code[], int sec[], int dim) {
 }
 
 
-
-
 void table(int a[][6], int num) {
     system("cls");
     cout<<" _____ _____ _____ _____       _____ _____ "<<endl;
@@ -183,6 +183,15 @@ void table(int a[][6], int num) {
 
 void tableColor(int a[][6], int num) {
     system("cls");
+	cout << "Ricorda:" << endl;	    
+	cout << "				1. blu" << endl;
+	cout << "				2. verde" << endl;
+	cout << "				3. azzurro" << endl;
+	cout << "				4. rosso" << endl;
+	cout << "				5. magenta" << endl;
+	cout << "				6. giallo" << endl;
+	cout << "				7. bianco" << endl;
+	cout << "				8. grigio" << endl;
     cout<<" _____ _____ _____ _____       _____ _____ "<<endl;
     for (int i=0; i<num; i++) {
         cout<<"|     |     |     |     |     |     |     |"<<endl;
@@ -208,7 +217,184 @@ void tableColor(int a[][6], int num) {
 }
 
 
+void main1() {
+	setlocale(LC_ALL, "");
+	system("cls");
+    srand(time(0));
+    int prove=12, fatte=0;
+    int tentativi[prove][6]; // Matrice dei tentativi
+    int secret[dim];
+	utente xx;
+	
+	cout << "Benvenuto in questa modalita' di gioco" << endl;
+	cout << "Inserisci il tuo nome: ";
+	cin >> xx.nome;
+	cout << "Inserisci la tua eta': ";
+	cin >> xx.anni;
+	xx.mod=1;
+	
+    cout << "codice segreto generato dal PC: ";
+    secretCode(secret, dim);
+
+    int codeut[dim];
+	cout<<"inserire il codice"<<endl;
+    inputCode(codeut, dim);
+    system("CLS");
+
+    for (int i=0; i<dim; i++) {
+        tentativi[0][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice
+    }
+    tentativi[0][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
+    tentativi[0][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
+    table(tentativi, 1); // Stampo la tabella con 1 sola riga
+
+    while (check(codeut, secret, dim) == false && prove>0) {
+    	inputCode(codeut, dim);
+        fatte++;
+        for (int i=0; i<dim; i++) {
+            tentativi[fatte][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice
+        }
+        tentativi[fatte][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
+        tentativi[fatte][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
+    	table(tentativi, fatte+1); // fatte+1 è il numero di righe da stampare 
+        prove--;
+	}
+    
+    char salvataggio;
+    ofstream fout;
+    if (prove>0) {
+	    cout << "Vuoi salvare la partita? Indica yes/no" << endl;
+	    cin >> salvataggio;
+	    if (salvataggio=='y') {
+	        fout.open ("ultima partita giocata.txt", ios::out);
+	        if (fout) {
+				fout << "Ricorda:" << endl;	    
+				fout << "				1. blu" << endl;
+				fout << "				2. verde" << endl;
+				fout << "				3. azzurro" << endl;
+				fout << "				4. rosso" << endl;
+				fout << "				5. magenta" << endl;
+				fout << "				6. giallo" << endl;
+				fout << "				7. bianco" << endl;
+				fout << "				8. grigio" << endl;
+				fout << endl;
+				fout << "Ciao " << xx.nome << " di anni " << xx.anni <<". Hai giocato nella modalita' numero " << xx.mod << endl;	
+				fout << "Hai indovinato il codice in " << 13-prove << " tentativi" << endl;       	
+	        	fout << "Ecco i tuoi tentativi per indovinare il codice" << endl;
+	            for (int i=0; i<fatte+1; i++) {
+	            	for (int j=0; j<4; j++)
+	            		fout << tentativi[i][j] << " ";	
+	            	fout << endl;
+				}
+				fout << endl << endl;
+				fout << "Il codice da indovinare era: ";
+				for (int i=0; i<dim; i++)
+					fout << secret[i] << " ";
+				fout << endl;        
+	        }
+	        else
+	            cout << "errore creazione file" << endl;
+	        fout.close();
+	    }
+	    if (salvataggio == 'n')
+	        cout << "grazie per aver giocato, alla prossima!" << endl;
+	}
+	else
+		cout<< endl << "hai finito i tentativi a tua disposizione, per questa volta ha vinto il computer!";
+        
+}
+
+
+void main2() {
+	setlocale(LC_ALL, "");
+	system("cls");
+    srand(time(0));
+    int prove=12, fatte=0;
+    int tentativi[prove][6];
+    int secret[dim];
+    utente xx;
+	
+	cout << "Benvenuto in questa modalita' di gioco" << endl;
+	cout << "Inserisci il tuo nome: ";
+	cin >> xx.nome;
+	cout << "Inserisci la tua eta': ";
+	cin >> xx.anni;
+	xx.mod=2;
+    
+    cout << "codice segreto generato dal PC: " << endl;
+    secretCodeColor(secret, dim);
+
+    int codeut[dim];
+	cout<<"inserire il codice"<<endl;
+    inputCodeColor(codeut, dim);
+    system("CLS");
+
+    for (int i=0; i<dim; i++) {
+        tentativi[0][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice, a riga 0
+    }
+    tentativi[0][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
+    tentativi[0][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
+    tableColor(tentativi, 1); // stampo la tabella con 1 riga
+
+    while (check(codeut, secret, dim) == false && prove>0) {
+    	inputCodeColor(codeut, dim);
+        fatte++;
+        for (int i=0; i<dim; i++) {
+            tentativi[fatte][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice e nelle righe pari a quanti sono i tentativi già eseguiti
+        }
+        tentativi[fatte][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
+        tentativi[fatte][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
+    	tableColor(tentativi, fatte+1); // fatte+1 è il numero di righe da stampare 
+        prove--;
+	}
+	
+	char salvataggio;
+    ofstream fout;
+    if (prove>0) {
+	    cout << "Vuoi salvare la partita? Indica yes/no" << endl;
+	    cin >> salvataggio;
+	    if (salvataggio=='y') {
+	        fout.open ("ultima partita giocata.txt", ios::out);
+	        if (fout) {
+				fout << "Ricorda:" << endl;	    
+				fout << "				1. blu" << endl;
+				fout << "				2. verde" << endl;
+				fout << "				3. azzurro" << endl;
+				fout << "				4. rosso" << endl;
+				fout << "				5. magenta" << endl;
+				fout << "				6. giallo" << endl;
+				fout << "				7. bianco" << endl;
+				fout << "				8. grigio" << endl;
+				fout << endl;
+				fout << "Ciao " << xx.nome << " di anni " << xx.anni <<". Hai giocato nella modalita' numero " << xx.mod << endl;	
+				fout << "Hai indovinato il codice in " << 13-prove << " tentativi" << endl;       	
+	        	fout << "Ecco i tuoi tentativi per indovinare il codice" << endl;
+	            for (int i=0; i<fatte+1; i++) {
+	            	for (int j=0; j<4; j++)
+	            		fout << tentativi[i][j] << " ";	
+	            	fout << endl;
+				}
+				fout << endl << endl;
+				fout << "Il codice da indovinare era: ";
+				for (int i=0; i<dim; i++)
+					fout << secret[i] << " ";
+				fout << endl;        
+	        }
+	        else
+	            cout << "errore creazione file" << endl;
+	        fout.close();
+	    }
+	    if (salvataggio == 'n')
+	        cout << "grazie per aver giocato, alla prossima!" << endl;
+	}
+	else
+		cout<< endl << "hai finito i tentativi a tua disposizione, per questa volta ha vinto il computer!";
+}
+
+
+
 int main () {
+	setlocale(LC_ALL, "");
 	ifstream fin;
 	string testo;
 	fin.open("ii.txt", ios::in);
@@ -222,78 +408,18 @@ int main () {
 		cout << "errore apertura file indicazioni iniziali.txt" << endl;
 	
 	fin.close();
-		
-		
+
 	int mod;
 	cout << "Inserisci la modalita' di gioco: " << endl;
 	cin >> mod;
+	while (mod!=1&&mod!=2) {
+		cout << "errore scegli tra la modalità 1 o 2" << endl;
+		cin >> mod;		
+	}
 	if (mod==1)
 		main1();
 	if (mod==2)
 		main2();
-}
 
-
-void main1() {
-	system("cls");
-    srand(time(0));
-    int prove=7, fatte=0;
-    int tentativi[prove][6]; // Matrice dei tentativi
-    int secret[dim];
-    cout << "codice segreto generato dal PC: ";
-    secretCode(secret, dim);
-    int codeut[dim];
-	cout<<"inserire il codice"<<endl;
-    inputCode(codeut, dim);
-    system("CLS");
-    for (int i=0; i<dim; i++) {
-        tentativi[0][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice
-    }
-    tentativi[0][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
-    tentativi[0][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
-    table(tentativi, 1); // 2, numero di righe da stampare 
-    while (check(codeut, secret, dim) == false && prove>0) {
-    	inputCode(codeut, dim);
-        fatte++;
-        for (int i=0; i<dim; i++) {
-            tentativi[fatte][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice
-        }
-        tentativi[fatte][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
-        tentativi[fatte][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
-    	table(tentativi, fatte+1); // fatte+1 è il numero di righe da stampare 
-        prove--;
-	}
-}
-
-
-void main2() {
-	system("cls");
-    srand(time(0));
-    int prove=7, fatte=0;
-    int tentativi[prove][6];
-    int secret[dim];
-    
-    cout << "codice segreto generato dal PC: " << endl;
-    secretCodeColor(secret, dim);
-    int codeut[dim];
-	cout<<"inserire il codice"<<endl;
-    inputCodeColor(codeut, dim);
-    system("CLS");
-    for (int i=0; i<dim; i++) {
-        tentativi[0][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice
-    }
-    tentativi[0][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
-    tentativi[0][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
-    tableColor(tentativi, 1); // 2, numero di righe da stampare 
-    while (check(codeut, secret, dim) == false && prove>0) {
-    	inputCodeColor(codeut, dim);
-        fatte++;
-        for (int i=0; i<dim; i++) {
-            tentativi[fatte][i]=codeut[i]; // Inserisco il codice utente nelle prime dim colonne della matrice
-        }
-        tentativi[fatte][4] = numGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti nelle prime dim colonne della matrice
-        tentativi[fatte][5] = posGiusti(codeut, secret, dim); // Inserisco il numero di numeri giusti in posizione giusta nelle prime dim colonne della matrice
-    	tableColor(tentativi, fatte+1); // fatte+1 è il numero di righe da stampare 
-        prove--;
-	}
+	return 0;
 }
